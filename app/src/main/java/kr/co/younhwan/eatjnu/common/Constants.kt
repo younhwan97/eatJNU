840f6d2d84375664f4c9bad27d41026a551b37a5
younhwan97 <younhwan0903@gmail.com>
2023-02-07 15:35:24 +0900

    [Add] BaseUrl address

--- app/src/main/java/kr/co/younhwan/eatjnu/common/Constants.kt
+++ app/src/main/java/kr/co/younhwan/eatjnu/common/Constants.kt
@@ -1,5 +1,6 @@
 package kr.co.younhwan.eatjnu.common
 
 object Constants {
-    const val BASE_URL = ""
+    const val BASE_URL = "http://ec2-15-164-250-158.ap-northeast-2.compute.amazonaws.com/API/"
+    
 }
