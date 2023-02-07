63e9c23d0f4796b5a1cc6207eba8e245a25abe24
younhwan97 <younhwan0903@gmail.com>
2023-02-07 16:24:11 +0900

    [Create] get place list use case

---
+++ app/src/main/java/kr/co/younhwan/eatjnu/domain/use_case/get_place_list/GetPlaceListUseCase.kt
@@ -0,0 +1,28 @@
+package kr.co.younhwan.eatjnu.domain.use_case.get_place_list
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kr.co.younhwan.eatjnu.common.Resource
+import kr.co.younhwan.eatjnu.data.remote.dto.toPlace
+import kr.co.younhwan.eatjnu.domain.model.Place
+import kr.co.younhwan.eatjnu.domain.repository.EatJnuRepository
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+class GetPlaceListUseCase @Inject constructor(
+    private val repository: EatJnuRepository
+) {
+
+    operator fun invoke(): Flow<Resource<List<Place>>> = flow {
+        try {
+            emit(Resource.Loading())
+            val places = repository.getPlaceList().map { it.toPlace() }
+            emit(Resource.Success(data = places))
+        } catch (e: HttpException) {
+            emit(Resource.Error(e.localizedMessage ?: "An expected error!"))
+        } catch (e: IOException) {
+            emit(Resource.Error("Couldn't reach server"))
+        }
+    }
+}
