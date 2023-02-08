3ea7a394222b5dcd4e2025f0eb1ea74c1d3b9f8a
younhwan97 <younhwan0903@gmail.com>
2023-02-08 22:31:44 +0900

    [Refactor] FilterScreen comp

--- app/src/main/java/kr/co/younhwan/eatjnu/presentation/place_list/components/FilterScreen.kt
+++ app/src/main/java/kr/co/younhwan/eatjnu/presentation/place_list/components/FilterScreen.kt
@@ -1,17 +1,16 @@
 package kr.co.younhwan.eatjnu.presentation.place_list.components
 
-import android.util.Log
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.ViewModel
 import kr.co.younhwan.eatjnu.domain.model.FilterInfo
 import kr.co.younhwan.eatjnu.presentation.place_list.PlaceListViewModel
 
+@SuppressLint("ModifierParameter")
 @Composable
 fun FilterScreen(
     filters: List<FilterInfo> = emptyList(),
@@ -19,24 +18,23 @@ fun FilterScreen(
     viewModel: PlaceListViewModel,
     modifier: Modifier = Modifier.fillMaxWidth()
 ) {
-    Log.d("temp", selectedFilterNum.toString())
+
     LazyRow(
-        modifier = Modifier.fillMaxWidth(),
-        contentPadding = PaddingValues(
-            start = 16.dp,
-            end = 16.dp
-        ),
-        horizontalArrangement = Arrangement.spacedBy(16.dp)
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        modifier = modifier,
     ) {
+
         itemsIndexed(items = filters) { index, item ->
+
             if (index == selectedFilterNum)
-                Filter(info = item, selected = true, index = index, onClickFilter = {
+                Filter(info = item, selected = true, index = index) {
                     viewModel.changeFilter(it)
-                })
+                }
             else
-                Filter(info = item, selected = false, index = index, onClickFilter = {
+                Filter(info = item, index = index) {
                     viewModel.changeFilter(it)
-                })
+                }
         }
     }
 }
