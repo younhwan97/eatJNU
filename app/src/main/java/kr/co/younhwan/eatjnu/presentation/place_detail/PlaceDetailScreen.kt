8c7073a2f95f572f8772b2278a13844737bfd9d2
younhwan97 <younhwan0903@gmail.com>
2023-02-10 22:10:17 +0900

    [Impl] PlaceDetailScreen Header

--- app/src/main/java/kr/co/younhwan/eatjnu/presentation/place_detail/PlaceDetailScreen.kt
+++ app/src/main/java/kr/co/younhwan/eatjnu/presentation/place_detail/PlaceDetailScreen.kt
@@ -1,7 +1,10 @@
 package kr.co.younhwan.eatjnu.presentation.place_detail
 
 import android.util.Log
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
@@ -9,14 +12,33 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.zIndex
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.skydoves.landscapist.ImageOptions
+import com.skydoves.landscapist.glide.GlideImage
+import kr.co.younhwan.eatjnu.R
+import kr.co.younhwan.eatjnu.domain.model.PlaceDetailInfo
 import kr.co.younhwan.eatjnu.presentation.Screen
+import kr.co.younhwan.eatjnu.presentation.place_detail.components.Header
 import kr.co.younhwan.eatjnu.presentation.supprot.ErrorScreen
 import kr.co.younhwan.eatjnu.presentation.supprot.LoadingScreen
 import kr.co.younhwan.eatjnu.presentation.supprot.MyTopAppBar
@@ -34,17 +56,18 @@ fun PlaceDetailScreen(
 
     /* UI */
     Column {
-        MyTopAppBar(
-            title = placeDetail.name,
-            navController = navController,
-            isVisibleHomeBtn = true
-        )
-
         if (isLoading) {
             // Loading
             LoadingScreen()
         } else if (placeDetail.id != -1) {
             // Success
+            Header(
+                placeDetail = placeDetail,
+                navController = navController
+            )
+
+            
+
 
         } else {
             // Error
--- app/src/main/java/kr/co/younhwan/eatjnu/presentation/supprot/MyTopAppBar.kt
+++ app/src/main/java/kr/co/younhwan/eatjnu/presentation/supprot/MyTopAppBar.kt
@@ -1,5 +1,6 @@
 package kr.co.younhwan.eatjnu.presentation.supprot
 
+import android.annotation.SuppressLint
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
@@ -9,19 +10,26 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.navigation.NavController
 import kr.co.younhwan.eatjnu.presentation.Screen
 
-
+@SuppressLint("ModifierParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MyTopAppBar(
     title: String = "",
+    containerColor: Color = Color.White,
     navController: NavController,
-    isVisibleHomeBtn: Boolean = false
+    isVisibleHomeBtn: Boolean = false,
+    modifier: Modifier = Modifier
 ) {
+    val iconColor = if (containerColor == Color.White.copy(0f)) Color.White else Color.Black
+
     CenterAlignedTopAppBar(
         title = {
             Text(
@@ -30,6 +38,9 @@ fun MyTopAppBar(
                 fontWeight = FontWeight.Bold
             )
         },
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = containerColor
+        ),
         navigationIcon = {
             IconButton(
                 onClick = {
@@ -38,7 +49,8 @@ fun MyTopAppBar(
             ) {
                 Icon(
                     imageVector = Icons.Filled.ArrowBack,
-                    contentDescription = null
+                    contentDescription = null,
+                    tint = iconColor
                 )
             }
         },
@@ -47,10 +59,12 @@ fun MyTopAppBar(
                 IconButton(onClick = { navController.navigate(Screen.HomeScreen.route) { popUpTo(0) } }) {
                     Icon(
                         imageVector = Icons.Filled.Home,
-                        contentDescription = null
+                        contentDescription = null,
+                        tint = iconColor
                     )
                 }
             }
-        }
+        },
+        modifier = modifier
     )
 }
